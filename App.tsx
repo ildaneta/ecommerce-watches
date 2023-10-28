e365fcfbea7699a92f7b9394d6311344c82b7521
ildaneta <iildaemanoely@gmail.com>
2023-10-28 19:19:29 +0200

    feat: declaring jost fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_500Medium,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
+
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Jost_400Regular,
+    Jost_500Medium,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Jost_400Regular' }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
