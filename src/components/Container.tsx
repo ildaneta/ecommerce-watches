288fa616ebfe66e9dd294061480075c4dacb3d91
ildaneta <iildaemanoely@gmail.com>
2023-10-29 13:26:05 +0100

    feat: create container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollView, View } from '@gluestack-ui/themed';
+import { Platform, SafeAreaView, StatusBar } from 'react-native';
+
+interface IContainerProps {
+  children: React.ReactNode;
+  needsScroll?: boolean;
+}
+
+const Container = ({
+  children,
+  needsScroll = false,
+}: IContainerProps): JSX.Element => {
+  const IS_ANDROID = Platform.OS === 'android';
+  const STATUS_BAR_HEIGHT = StatusBar.currentHeight;
+
+  return (
+    <>
+      <SafeAreaView
+        style={{ paddingTop: IS_ANDROID ? STATUS_BAR_HEIGHT : 0 }}
+      />
+
+      <View flex={1} pt={'$6'} px={'$4'}>
+        {needsScroll ? <ScrollView>{children}</ScrollView> : <>{children}</>}
+      </View>
+    </>
+  );
+};
+
+export default Container;
