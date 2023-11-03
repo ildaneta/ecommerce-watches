985250f221494d4fb213609f6941f07ae3a4442a
ildaneta <iildaemanoely@gmail.com>
2023-11-03 12:57:15 +0100

    refactor: insert url into images

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,67 +1,67 @@
-import PetiteCherry from '../assets/petite-cherry.svg';
-import ClassicMawes from '../assets/classic-mawes.svg';
-import ClassicOxford from '../assets/classic-oxford.svg';
-import PetiteRosewater from '../assets/petite-rosewater.svg';
-import ClassicSouthampton from '../assets/classic-southampton.svg';
-import ClassicCornwall from '../assets/classic-cornwall.svg';
-import ClassicRoselyn from '../assets/classic-roselyn.svg';
-import PetiteSuffolk from '../assets/petite-suffolk.svg';
-
 export const products = [
   {
     id: '1',
     name: 'Petite Cherry',
     price: '209.00',
-    image: PetiteCherry,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/2d98b4c3-b5ce-4d97-b0f1-ed2da34b46b3',
     quantity: 4,
   },
   {
     id: '2',
     name: 'Classic Mawes',
     price: '199.00',
-    image: ClassicMawes,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/0e4aa713-6398-4928-9972-76e8edb7fb0c',
     quantity: 3,
   },
   {
     id: '3',
     name: 'Classic Oxford',
     price: '209.00',
-    image: ClassicOxford,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/2732fd1e-c645-4c11-a63d-47466a4059c2',
     quantity: 2,
   },
   {
     id: '4',
     name: 'Petite Rosewater',
     price: '209.00',
-    image: PetiteRosewater,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/8e62da11-48ce-4208-a943-34f47fb6ac91',
     quantity: 6,
   },
   {
     id: '5',
     name: 'Classic Southampton',
     price: '209.00',
-    image: ClassicSouthampton,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/9a13ef36-4656-4cdd-bec1-02b9d9e84828',
     quantity: 3,
   },
   {
     id: '6',
     name: 'Classic Cornwall',
     price: '209.00',
-    image: ClassicCornwall,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/257f272f-2560-423a-91c9-ae5512e74778',
+
     quantity: 7,
   },
   {
     id: '7',
     name: 'Classic Roselyn',
     price: '209.00',
-    image: ClassicRoselyn,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/69560f56-58e7-4e59-b07a-73565e55daa1',
     quantity: 3,
   },
   {
     id: '8',
     name: 'Petite Suffolk',
     price: '209.00',
-    image: PetiteSuffolk,
+    image:
+      'https://github.com/ildaneta/ecommerce-watches/assets/21963291/98fb3bd9-b5d9-40df-bb6e-9a938da93b07',
     quantity: 4,
   },
 ];
